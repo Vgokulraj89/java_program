4bbf9a20b9f141ca9689569681de70cef7c69794
Vgokulraj89 <128820711+Vgokulraj89@users.noreply.github.com>
2023-03-24 22:31:28 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,8 @@
 public class Fibonacci {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int s=sc.nextInt();
 		int a=0;
 		int b=1;
-		for(int i=0;i<=s;i++) {
+		for(int i=0;i<10;i++) {
 			System.out.println(a);
 			int c=a+b;
 			a=b;
